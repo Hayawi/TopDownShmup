c9200f37e805965f5ec47369f0328274e00f9615
Hayawi <yahya.ismail@hotmail.ca>
2016-03-17 18:53:58 -0400

    Added player assets and playercontroller
    still need to fix rotation

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float characterSpeed;
+	public Camera characterCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetButton("Horizontal")) {
+			GetComponent<Transform> ().Translate(new Vector3(characterSpeed * Input.GetAxis("Horizontal"), 0, 0));
+		}
+		if (Input.GetButton ("Vertical")) {
+			GetComponent<Transform> ().Translate(new Vector3(0, characterSpeed * Input.GetAxis("Vertical"), 0));
+		}
+		FaceMouse();
+	}
+
+	void FaceMouse () {
+		Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -25);
+		Vector3 lookPos = characterCamera.ScreenToWorldPoint(mousePos);
+		lookPos = lookPos - transform.position;
+		float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+	}
+}
