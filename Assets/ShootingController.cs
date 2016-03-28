6bac6696bbdcc447da6546b22bd153077796a0dd
Hayawi <yahya.ismail@hotmail.ca>
2016-03-28 18:49:02 -0400

    working on the shooting capabilities

--- Assets/ShootingController.cs
+++ Assets/ShootingController.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class ShootingController : MonoBehaviour {
 
+	public GameObject characterBody;
+
+	public Rigidbody2D bullet;
+
+	public float bulletSpeed = 10f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +16,17 @@ public class ShootingController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButtonDown ("Shoot")) {
+			spawnNewBullet();
+		}
+	}
+
+	void spawnNewBullet() {
+		Rigidbody2D bulletClone = (Rigidbody2D)Instantiate (bullet, characterBody.GetComponent<Transform> ().position, (characterBody.GetComponent<Transform> ().rotation));
+		float angle = bulletClone.transform.rotation.eulerAngles.z ;
+		bulletClone.GetComponent<Transform>().rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+
+		bulletClone.velocity =  new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * bulletSpeed;
+		print (bulletClone.transform.rotation.eulerAngles);
 	}
 }
