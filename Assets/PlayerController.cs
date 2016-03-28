e42d8cfed7467acd4dac7a97697276f48a972de4
Hayawi <yahya.ismail@hotmail.ca>
2016-03-27 23:03:58 -0400

    Fixed movement and rotation

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,6 +5,9 @@ public class PlayerController : MonoBehaviour {
 
 	public float characterSpeed;
 	public Camera characterCamera;
+    public GameObject characterBody;
+
+    private float characterRotation;
 
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -22,7 +25,8 @@ public class PlayerController : MonoBehaviour {
 		Vector3 lookPos = characterCamera.ScreenToWorldPoint(mousePos);
 		lookPos = lookPos - transform.position;
 		float angle = (Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg) - 90f;
-		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
-		characterCamera.GetComponent<Transform> ().rotation = Quaternion.AngleAxis (0, Vector3.forward);
+		//characterBody.GetComponent<Transform>().rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+        characterRotation = angle;
+		//characterCamera.GetComponent<Transform> ().rotation = Quaternion.AngleAxis (0, Vector3.forward);
 	}
 }
