ef92b80deebcc799d174e9ffe79bdebaff7e2649
Hayawi <yahya.ismail@hotmail.ca>
2016-03-31 16:40:07 -0400

    Starting to add map generation

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class MapGenerator : MonoBehaviour {
 
+	public Rigidbody2D floorTile;
+
+	public float complexSizeX;
+	public float complexSizeY;
+
 	// Use this for initialization
 	void Start () {
-	
+		float xStart = (complexSizeX / 2) * 4.81f;
+		float yStart = (complexSizeY / 2) * 4.81f;
+		for (int i = 0; i < complexSizeX; i++) {
+			for (int j = 0; j < complexSizeY; j++) {
+				spawnNewFloorTile ((-xStart) + (i * 4.81f), (-yStart) + (j * 4.81f)); 
+			}
+		}
 	}
-	
+
+	void spawnNewFloorTile(float x, float y) {
+		Rigidbody2D floorTileClone = (Rigidbody2D)Instantiate (floorTile, new Vector3 (x, y, 0), floorTile.GetComponent<Transform> ().rotation);
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
