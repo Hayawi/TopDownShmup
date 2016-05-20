d9b4f3bf3e407a04d57b481c5878fea0722b89d8
Hayawi <yahya.ismail@hotmail.ca>
2016-05-20 09:42:43 -0400

    Working on lighting

---
+++ Assets/Scripts/FlashlightController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashlightController : MonoBehaviour {
+
+    public GameObject flashlight;
+
+    GameObject characterRotation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
